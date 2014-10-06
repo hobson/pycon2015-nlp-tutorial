2a49bbc0988915d2aeacc78f59ea506aa4c62d89
Hobson Lane <laneh@sharplabs.com>
2014-10-06 14:29:39 -0700

    slides drafted, python to autogenerate slides from outline

--- docs/schedule.py
+++ docs/schedule.py
@@ -1,17 +1,20 @@
-# calc_schedule.py
+#!/usr/bin/env python
+# schedule.py
+
+import re
+import collections
+RE_TIME = re.compile(r'([^(]*)\(\s*(\d+)\s*(?:min)?\s*\)([^(]*)', re.IGNORECASE)
 
 def schedule(path='outline.md'):
     """Process an outline in Markdown format, extracting the time estimates for each line"""
-    import re
-    RE_TIME = re.compile(r'([^(]*)\(\s*(\d+)\s*(?:min)?\s*\)([^(]*)', re.IGNORECASE)
 
-    sections, parent, i = [], 0, 1
+    sections, parent, i = collections.OrderedDict(), 0, 1
     with open(path, 'r') as f:
         for line in f:
             line = line.strip()
             if not line:
                 continue
-            matches = RE_TIME.match()
+            matches = RE_TIME.match(line)
             if matches:
                 groups = [g.strip().strip('.').strip() for g in matches.groups()]
                 try:
@@ -26,3 +29,17 @@ def schedule(path='outline.md'):
     return sections
 
 
+def generate_markdown(sections):
+    for i, section in sections.iteritems():
+        if section.get('name', None):
+            slide = '---\n'
+            slide += 'title: {0}'.format(section['name'])
+
+
+def print_markdown(path='outline.md'):
+    for slide in generate_markdown(schedule(path)):
+        print slide
+
+
+if __name__ == '__main__':
+    print_markdown()
