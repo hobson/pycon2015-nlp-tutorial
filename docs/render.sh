c3fbc66a3e2843750f67bc49062cc7e686b8e6fb
Hobson Lane <laneh@sharplabs.com>
2014-10-06 16:20:03 -0700

    ran render.sh. estiamted time now 4.4 hr

--- docs/render.sh
+++ docs/render.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-python schedule.py > slides/middle.md && cat slides/front.md middle.md slides/back.md > slides/slides.md && cd slides && slidedeck render && cd ..
+python schedule.py > slides/middle.md && cat slides/front.md slides/middle.md slides/back.md > slides/slides.md && cd slides && slidedeck render && cd ..
